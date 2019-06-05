d9e00049b9541a61dd42b0619783c7ab9154dc37
sultandaurenbek <42887144+sultandaurenbek@users.noreply.github.com>
2019-06-05 12:28:15 +0600

    Update 7.swift

--- 7.swift
+++ 7.swift
@@ -1,13 +1,14 @@
 class Solution {
-    func reverseString(_ s: String) -> String {
-        var index = s.endIndex
-        var result = ""
+   func reverseString(_ s: inout [Character]) {
+        var left = s.count / 2 - 1
+        var right = s.count % 2 == 0 ? left + 1 : left + 2
         
-        while index != s.startIndex {
-            index = s.index(before: index)
-            result.append(s[index])
+        while left >= 0{
+            var temp = s[left]
+            s[left] = s[right]
+            s[right] = temp
+            left -= 1
+            right += 1
         }
-        return result
     }
-}
 }
